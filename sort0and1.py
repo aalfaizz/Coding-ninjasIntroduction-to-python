4ae31367b9b08206fe912cc3d2d8f2de457907be
aalfaizz <63657794+aalfaizz@users.noreply.github.com>
2021-01-17 12:51:47 +0530

    Update sort0and1.py

--- sort0and1.py
+++ sort0and1.py
@@ -19,3 +19,35 @@ while a<(len(list1)):
     a = a + 1
 print(list1)
         
+#Another Approach using function
+
+def sortzeroesandone(arr,n):
+    
+    nextzero = 0
+    a = 0
+    while a<(len(arr)):
+        if arr[a] == 0:
+            temp = arr[nextzero]
+            arr[nextzero] = arr[a]
+            arr[a] = temp
+            nextzero += 1
+        a = a + 1
+def takeinput():
+    n = int(input())
+    arr = []
+    i = 0
+    while i<n:
+        curr = int(input())
+        arr.append(curr)
+        i = i + 1
+    return arr, n
+def printlist(arr,n):
+    for j in range(n):
+        print(arr[j], end = " ")
+    print()
+    
+
+arr, n = takeinput()
+sortzeroesandone(arr, n)
+printlist(arr, n)
+print()
